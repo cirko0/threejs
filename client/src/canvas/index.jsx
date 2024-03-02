35fc226f38e28150d8179f089bbd0eb148b06885
cirko0 <cirko007ivan@gmail.com>
2024-03-02 12:02:32 +0100

    add 3DCanvas and also initialized BackDrop, CameraRig and Shirt component

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { Environment, Center } from "@react-three/drei";
 
-const Canvas = () => {
-  return <div>Canvas</div>;
+import Shirt from "./Shirt";
+import BackDrop from "./BackDrop";
+import CameraRig from "./CameraRig";
+
+const CanvasModel = () => {
+  return (
+    <Canvas>
+      {/*eslint-disable-next-line react/no-unknown-property */}
+      <ambientLight intensity={0.5} />
+      <Environment preset="city" />
+      <CameraRig>
+        <BackDrop />
+        <Center>
+          <Shirt />
+        </Center>
+      </CameraRig>
+    </Canvas>
+  );
 };
 
-export default Canvas;
+export default CanvasModel;
